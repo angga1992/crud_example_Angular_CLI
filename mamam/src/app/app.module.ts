a817387f4d11f03c3345229edec9209b34a86ef8
Angga Mulya Sasmita <Angga Mulya Sasmita>
2017-03-14 10:48:18 +0700

    update

--- mamam/src/app/app.module.ts
+++ mamam/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import {DataTableModule} from "angular2-datatable";
 import { MaterialModule } from '@angular/material';
 import { DatepickerModule } from 'angular2-material-datepicker';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent,
